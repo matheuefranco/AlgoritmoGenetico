8e2a9e26f12885c0895893d5520a321a001c1aa7
matheuefranco <matheusefranco@gmail.com>
2024-04-26 07:34:09 -0300

    Aula 25-04

--- src/agjava2024/AGJava2024.java
+++ src/agjava2024/AGJava2024.java
@@ -4,7 +4,10 @@ package agjava2024;
 public class AGJava2024 {
 
     public static void main(String[] args) {
-       AlgoritmoGenetico meuAg = new AlgoritmoGenetico(20);
+        int populacao = 20;
+        double limitePeso = 8;
+       AlgoritmoGenetico meuAg = 
+               new AlgoritmoGenetico(populacao,limitePeso);
        meuAg.carregaArquivo("dados.csv");
        meuAg.executar();
     }
--- src/agjava2024/AlgoritmoGenetico.java
+++ src/agjava2024/AlgoritmoGenetico.java
@@ -6,16 +6,25 @@ import java.util.*;
 public class AlgoritmoGenetico {
     private int tamPopulacao;
     private int tamCromossomo=0;
+    private double capacidade;
     private ArrayList<Produto> produtos = new ArrayList<>();
     private ArrayList<ArrayList> populacao = new ArrayList<>();
-    
-    public AlgoritmoGenetico(int tamanhoPopulacao){
+    private ArrayList<Integer> roletaVirtual = new ArrayList<>();
+
+    public AlgoritmoGenetico(int tamanhoPopulacao, double capacidadeMochila){
         this.tamPopulacao = tamanhoPopulacao;
+        this.capacidade = capacidadeMochila;
     }
     
     public void executar(){
-        criarPopulacao();
-        System.out.println("População:\n"+this.populacao);
+        this.criarPopulacao();
+        this.gerarRoleta();
+        System.out.println("Selecionado:"+roleta());
+        for(int i=0;i<this.tamPopulacao;i++){
+            System.out.println("Cromossomo "+i+":"+populacao.get(i));
+            System.out.println("Avaliacao:"+fitness(populacao.get(i)));
+        }
+
     }
     
     public void carregaArquivo(String fileName){
@@ -51,11 +60,47 @@ public class AlgoritmoGenetico {
          }// fim for
          return novoCromossomo;
      }
-     
+  //--------------------------   
      private void criarPopulacao(){
          for(int i=0;i<this.tamPopulacao;i++)
              this.populacao.add(criarCromossomo());
      }
-
+  //----------------------------------
+   private double fitness(ArrayList<Integer> cromossomo){
+       double pesoTotal=0, valorTotal=0;
+       for(int i=0;i<this.tamCromossomo;i++){
+           if(cromossomo.get(i)==1){
+               pesoTotal+=produtos.get(i).getPeso();
+               valorTotal+=produtos.get(i).getValor();
+           }// fim if teste se leva
+       }
+       if(pesoTotal<=this.capacidade)
+           return valorTotal;
+       else
+           return 0;
+   }
+   private void gerarRoleta(){
+       ArrayList<Double> fitnessIndividuos = new
+           ArrayList<>();
+       double totalFitness=0;
+       for(int i=0;i<this.tamPopulacao;i++){
+           fitnessIndividuos.add(i,fitness(this.populacao.get(i)));
+           totalFitness+=fitnessIndividuos.get(i);
+       }
+       System.out.println("Soma total fitness:"+totalFitness);
+       System.out.println("Notas:"+fitnessIndividuos);
+       for(int i=0;i<this.tamPopulacao;i++){
+           double qtdPosicoes = (fitnessIndividuos.get(i)/totalFitness)*1000;
+           for(int j=0;j<=qtdPosicoes;j++)
+               roletaVirtual.add(i);
+       }// fim for i
+       System.out.println("Roleta Virtual:"+roletaVirtual);
+   }// fim gerarRoleta
+   
+   private int roleta(){
+       Random r = new Random();
+       int selecionado = r.nextInt(roletaVirtual.size());
+       return roletaVirtual.get(selecionado);
+   }// fim roleta
     
 }
